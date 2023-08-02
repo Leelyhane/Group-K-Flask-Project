9bb371dd689c775314c5c5f30b9c4cd075c03b48
landrian61 <luswataandrew190@gmail.com>
2023-08-02 13:27:38 +0300

    Changes in form logic

--- argon-dashboard-flask-master/apps/authentication/routes.py
+++ argon-dashboard-flask-master/apps/authentication/routes.py
@@ -4,7 +4,6 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask import Flask, render_template, request, redirect, url_for
-from forms import SubmitJobResume
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import (
     current_user,
@@ -16,7 +15,7 @@ from functools import wraps
 from apps import db, login_manager
 from apps.authentication import blueprint
 from apps.authentication.forms import LoginForm, CreateAccountForm, JobListings, Internships, SubmitInternResume, SubmitJobResume
-from apps.authentication.models import Users, Job_listings
+from apps.authentication.models import Users, Job_listings, Internships, Job_resumes, Intern_resumes
 
 from apps.authentication.util import verify_pass
 
@@ -104,61 +103,86 @@ def logout():
     return redirect(url_for('authentication_blueprint.login'))
 
 
-@blueprint.route('/add-job')
+@blueprint.route('/add-job', methods=['POST'])
 def add_job():
-    # create_job_form = JobListings(request.form)
     if request.method == "POST":
-        if 'job_name' in request.form and 'company_name' in request.form and 'job_description' in request.form and 'job-type' in request.form and 'application_deadline' in request.form:
+        form = JobListings(request.form)
+        if form.validate():  # Make sure to validate the form
             # Get the form data submitted by the admin
-            job_name = request.form['job_name']
-            company_name = request.form['company_name']
-            job_description = request.form['job_description']
-            job_category = request.form['job_type']
-            application_deadline = request.form['application_deadline']
-
-            # Convert Date Strings to Python date objects
-            from datetime import datetime
-            deadline = datetime.strptime(
-                application_deadline, '%Y-%m-%').date()
-
-            # Insert the job listing details into the database
-            job = Job_listings(**request.form)
-            db.session.add(job)
-            db.session.commit()
-
-            # Redirect to the available jobs page after successful submission
-            return render_template('/home/available-jobs.html')
-
-        # For GET requests, render the add_job.html template with an empty form
-    return render_template('/home/add-job.html')
-
-
-@blueprint.route('/add-intern', methods=['POST'])
-def add_intern():
-    if request.method == "POST":
-        if 'job_name' in request.form and 'company_name' in request.form and 'job_description' in request.form and 'job_type' in request.form and 'application_deadline' in request.form:
-            # Get the form data submitted by the admin
-            job_name = request.form['job_name']
-            company_name = request.form['company_name']
-            job_description = request.form['job_description']
-            job_category = request.form['job_type']
-            application_deadline = request.form['application_deadline']
+            job_name = form.job_name.data
+            company_name = form.company.data
+            job_description = form.job_description.data
+            job_category = form.job_category.data
+            location = form.location.data
+            application_deadline = form.application_deadline.data
+            company_logo = form.company_logo.data  # Get the company_logo field data
+            company_url = form.company_url.data    # Get the company_url field data
 
             # Convert Date Strings to Python date objects
             from datetime import datetime
             deadline = datetime.strptime(
                 application_deadline, '%Y-%m-%d').date()
 
-            # Insert the intern listing details into the database
-            intern = Internships(**request.form)
-            db.session.add(intern)
+            # Insert the job listing details into the database
+            job_listing = Job_listings(
+                job_name=job_name,
+                company=company_name,
+                description=job_description,
+                location=location,
+                job_type=job_category,
+                application_deadline=deadline,
+                company_logo=company_logo,  # Add the company_logo data to the model
+                company_url=company_url     # Add the company_url data to the model
+            )
+            db.session.add(job_listing)
             db.session.commit()
 
-            # Redirect to the available internships page after successful submission
-            return redirect(url_for('/home/available-internships.html'))
+            # Redirect to the available jobs page after successful submission
+            return redirect(url_for('home_blueprint.available_jobs'))
 
-    # For GET requests, render the add_intern.html template with an empty form
-    return render_template('home/add-intern.html')
+    # For GET requests or invalid form submissions, render the add_job.html template with the form
+    return render_template('/home/add-job.html', form=JobListings())
+
+
+@blueprint.route('/add-intern', methods=['POST'])
+def add_internship():
+    if request.method == "POST":
+        form = Internships(request.form)
+        if form.validate_on_submit():
+            # Get the form data submitted by the admin
+            job_name = form.intern_name.data
+            company_name = form.company.data
+            internship_description = form.internship_description.data
+            job_category = form.job_category.data
+            location = form.location.data
+            application_deadline = form.application_deadline.data
+            logo = form.company_logo.data  # Get the company_logo field data
+            url = form.company_url.data    # Get the company_url field data
+
+            # Convert Date Strings to Python date objects
+            from datetime import datetime
+            deadline = datetime.strptime(
+                application_deadline, '%Y-%m-%d').date()
+
+            # Insert the internship details into the database
+            internship = Internships(
+                job_name=job_name,
+                company_name=company_name,
+                job_description=internship_description,
+                job_category=job_category,
+                locaion=location,
+                application_deadline=deadline,
+                company_logo=logo,  # Add the company_logo data to the model
+                company_url=url     # Add the company_url data to the model
+            )
+            db.session.add(internship)
+            db.session.commit()
+
+            # Redirect to the available jobs page after successful submission
+            return redirect(url_for('home_blueprint.available_internships'))
+
+    # For GET requests or invalid form submissions, render the add_job.html template with the form
+    return render_template('/home/add-intern.html', form=Internships())
 
 
 @blueprint.route('/submit-job-resume', methods=['GET', 'POST'])
@@ -171,17 +195,21 @@ def submit_job_resume():
         email = form.email.data
         resume_file = form.resume_file.data
 
-        # Add your code to process the resume submission here
+        # Create a new JobResume instance and save the data to the database
+        job_resume = Job_resumes(
+            name=name, email=email, resume_file=resume_file)
+        db.session.add(job_resume)
+        db.session.commit()
 
         # Redirect to a confirmation page after successful submission
-        return redirect(url_for('job_resume_submission_confirmation'))
+        return render_template('/home/index.html')
 
     # For GET requests or invalid form submissions, render the submit_job_resume.html template with the form
     return render_template('submit-job-resume.html', form=form)
 
 
 @blueprint.route('/submit-intern-resume', methods=['GET', 'POST'])
-def submit_job_resume():
+def submit_intern_resume():
     form = SubmitInternResume()
 
     if form.validate_on_submit():
@@ -190,16 +218,20 @@ def submit_job_resume():
         email = form.email.data
         resume_file = form.resume_file.data
 
-        # Add your code to process the resume submission here
+        # Create a new JobResume instance and save the data to the database
+        job_resume = Intern_resumes(
+            name=name, email=email, resume_file=resume_file)
+        db.session.add(job_resume)
+        db.session.commit()
 
         # Redirect to a confirmation page after successful submission
-        return redirect(url_for('intern_resume_submission_confirmation'))
+        return render_template('/home/index.html')
 
     # For GET requests or invalid form submissions, render the submit_job_resume.html template with the form
     return render_template('submit-intern-resume.html', form=form)
 
-
 # Errors
+
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
